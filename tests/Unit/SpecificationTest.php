244c6b9f90addebc4ecb04803494f99c9d326cc6
Serhii Andriichuk <andriichuk29@gmail.com>
2021-12-26 07:59:10 +0200

    Add tests bootstrap

---
+++ tests/Unit/SpecificationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Andriichuk\Enviro\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SpecificationTest extends TestCase
+{
+    public function testCreation()
+    {
+        $this->assertTrue(true);
+    }
+}
