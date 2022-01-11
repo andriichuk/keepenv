a61455ef70f4730af7ebbd7cca76a82327bad23a
Serhii Andriichuk <andriichuk29@gmail.com>
2022-01-11 06:32:55 +0200

    Remove psalm array definition

--- src/Specification/SpecificationBuilderInterface.php
+++ src/Specification/SpecificationBuilderInterface.php
@@ -6,8 +6,5 @@ namespace Andriichuk\KeepEnv\Specification;
 
 interface SpecificationBuilderInterface
 {
-    /**
-     * @psalm-param array{version: string, environments: array} $rawDefinition
-     */
     public function build(array $rawDefinition): Specification;
 }
