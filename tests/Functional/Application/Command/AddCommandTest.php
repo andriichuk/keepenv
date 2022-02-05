ae34bc8a8a54cf91ba98fee59079aa01c446a0a5
Serhii Andriichuk <andriichuk29@gmail.com>
2022-02-05 21:42:11 +0200

    Improve add command tests

--- tests/Functional/Application/Command/AddCommandTest.php
+++ tests/Functional/Application/Command/AddCommandTest.php
@@ -59,8 +59,179 @@ class AddCommandTest extends TestCase
         ]);
 
         $this->commandTester->assertCommandIsSuccessful();
+        $this->assertEquals(
+            "APP_NAME=KeepEnv\nDEMO_MODE=ON\n",
+            file_get_contents($this->rootFolder->getChild('.env')->url()),
+        );
         $this->assertFileEquals(
             dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_expected.yaml',
+            $this->rootFolder->getChild('keepenv.yaml')->url(),
+        );
+    }
+
+    public function testCommandCanValidateAndAskNewValueOnFail(): void
+    {
+        $this->rootFolder->addChild((new vfsStreamFile('.env'))->setContent('APP_NAME=KeepEnv'));
+        $this->rootFolder->addChild(
+            (new vfsStreamFile('keepenv.yaml'))
+                ->setContent(
+                    file_get_contents(dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_init.yaml')
+                )
+        );
+
+        $this->commandTester->setInputs([
+            'DEMO_MODE', // name
+            'Demo mode.', // description
+            'yes', // required
+            'no', // export
+            'no', // system
+            '2', // boolean type
+            'Y', // wrong value
+            'ON', // valid value
+            'no', // stop adding
+        ]);
+
+        $this->commandTester->execute([
+            '--env' => 'common',
+            '--target-env-file' => $this->rootFolder->getChild('.env')->url(),
+            '--spec' => $this->rootFolder->getChild('keepenv.yaml')->url(),
+        ]);
+
+        $this->commandTester->assertCommandIsSuccessful();
+        $this->assertEquals(
+            "APP_NAME=KeepEnv\nDEMO_MODE=ON\n",
+            file_get_contents($this->rootFolder->getChild('.env')->url()),
+        );
+        $this->assertFileEquals(
+            dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_expected.yaml',
+            $this->rootFolder->getChild('keepenv.yaml')->url(),
+        );
+    }
+
+    public function testCommandCanPreventOfAddingSameVariable(): void
+    {
+        $this->rootFolder->addChild((new vfsStreamFile('.env'))->setContent('APP_NAME=KeepEnv'));
+        $this->rootFolder->addChild(
+            (new vfsStreamFile('keepenv.yaml'))
+                ->setContent(
+                    file_get_contents(dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_init.yaml')
+                )
+        );
+
+        $this->commandTester->setInputs([
+            'APP_NAME', // name
+            'Application name.', // description
+            'yes', // required
+            'no', // export
+            'no', // system
+            '0', // string type
+            'KeepEnv', // value
+            'no', // stop adding
+        ]);
+
+        $this->commandTester->execute([
+            '--env' => 'common',
+            '--target-env-file' => $this->rootFolder->getChild('.env')->url(),
+            '--spec' => $this->rootFolder->getChild('keepenv.yaml')->url(),
+        ]);
+
+        $this->commandTester->assertCommandIsSuccessful();
+        $this->assertEquals(
+            "APP_NAME=KeepEnv",
+            file_get_contents($this->rootFolder->getChild('.env')->url()),
+        );
+        $this->assertFileEquals(
+            dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_init.yaml',
+            $this->rootFolder->getChild('keepenv.yaml')->url(),
+        );
+    }
+
+    public function testCommandCanAddSeveralVariables(): void
+    {
+        $this->rootFolder->addChild((new vfsStreamFile('.env'))->setContent('APP_NAME=KeepEnv'));
+        $this->rootFolder->addChild(
+            (new vfsStreamFile('keepenv.yaml'))
+                ->setContent(
+                    file_get_contents(dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_init.yaml')
+                )
+        );
+
+        $this->commandTester->setInputs([
+            'APP_LOCALE', // name
+            'Application default locale.', // description
+            'yes', // required
+            'no', // export
+            'no', // system
+            '0', // string type
+            'en', // value
+
+            'yes', // continue adding
+
+            'APP_TIMEZONE', // name
+            'Application default timezone.', // description
+            'yes', // required
+            'no', // export
+            'no', // system
+            '0', // string type
+            'UTC', // value
+            'no', // stop adding
+        ]);
+
+        $this->commandTester->execute([
+            '--env' => 'common',
+            '--target-env-file' => $this->rootFolder->getChild('.env')->url(),
+            '--spec' => $this->rootFolder->getChild('keepenv.yaml')->url(),
+        ]);
+
+        $this->commandTester->assertCommandIsSuccessful();
+        $this->assertEquals(
+            "APP_NAME=KeepEnv\nAPP_LOCALE=en\nAPP_TIMEZONE=UTC\n",
+            file_get_contents($this->rootFolder->getChild('.env')->url()),
+        );
+        $this->assertFileEquals(
+            dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_multiple_expected.yaml',
+            $this->rootFolder->getChild('keepenv.yaml')->url(),
+        );
+    }
+
+    public function testCommandCanAskForEnumOptions(): void
+    {
+        $this->rootFolder->addChild((new vfsStreamFile('.env'))->setContent('APP_NAME=KeepEnv'));
+        $this->rootFolder->addChild(
+            (new vfsStreamFile('keepenv.yaml'))
+                ->setContent(
+                    file_get_contents(dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_init.yaml')
+                )
+        );
+
+        $this->commandTester->setInputs([
+            'APP_ENV', // name
+            'Application environment name.', // description
+            'yes', // required
+            'no', // export
+            'no', // system
+            '3', // enum type
+            'dev', // first enum option
+            'yes', // continue adding enum options
+            'prod', // second enum option
+            'no', // stop adding enum options
+            'dev', // value
+            'no', // stop adding
+        ]);
+
+        $this->commandTester->execute([
+            '--env' => 'common',
+            '--target-env-file' => $this->rootFolder->getChild('.env')->url(),
+            '--spec' => $this->rootFolder->getChild('keepenv.yaml')->url(),
+        ]);
+
+        $this->commandTester->assertCommandIsSuccessful();
+        $this->assertEquals(
+            "APP_NAME=KeepEnv\nAPP_ENV=dev\n",
+            file_get_contents($this->rootFolder->getChild('.env')->url()),
+        );
+        $this->assertFileEquals(
+            dirname(__DIR__, 3) . '/fixtures/case_10/keepenv_enum_expected.yaml',
             $this->rootFolder->getChild('keepenv.yaml')->url(),
         );
     }
